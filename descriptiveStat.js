0f00d2cccd0d9730ff3975a78e7c576d51be2b0e
Prevail Uzodinma <prevailuzodinma@gmail.com>
2024-01-26 12:12:54 +0100

    attempting the static class calcMedian

--- descriptiveStat.js
+++ descriptiveStat.js
@@ -9,5 +9,15 @@ class DataSet {
     console.log(mean);
     return mean;
   }
+
+  static calcMedian(array){
+    // first ensure that the array is sorted
+    array = array.sort((a, b) => a - b);
+    
+    let length = array.length;
+    let middleIndex = Math.floor(array.length / 2);
+    let median;
+}
+
 }
 DataSet.calcMean([1, 2, 5, 6, 7]);
