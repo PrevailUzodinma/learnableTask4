6bd31434b02f9ea9ed82bfd9ca48e95c00ed08f3
Prevail Uzodinma <prevailuzodinma@gmail.com>
2024-01-26 12:15:37 +0100

    just wrote code to test each case of when array length is even or odd

--- descriptiveStat.js
+++ descriptiveStat.js
@@ -14,9 +14,25 @@ class DataSet {
     // first ensure that the array is sorted
     array = array.sort((a, b) => a - b);
     
+    // capture the length of my array and the middle value index in variables
     let length = array.length;
     let middleIndex = Math.floor(array.length / 2);
     let median;
+
+    //testcase, if it is an even or odd length of values, cos median would differ
+
+    //if array length is odd
+    if (length % 2 !== 0){
+        median = array[middleIndex]
+        console.log(median);
+        return median
+    }
+    // if array length is even
+    else {
+        median = (array[middleIndex] + array[middleIndex - 1]) / 2
+        console.log(median);
+        return median
+    }
 }
 
 }
