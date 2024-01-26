41c6f2bfa051a7641f37a5e6fcf444b6a650d3a6
Prevail Uzodinma <prevailuzodinma@gmail.com>
2024-01-26 13:34:40 +0100

    I created an object {key: value} to map the array as Element: how many times it occurs}

--- descriptiveStat.js
+++ descriptiveStat.js
@@ -10,10 +10,10 @@ class DataSet {
     return mean;
   }
 
-  static calcMedian(array){
+  static calcMedian(array) {
     // first ensure that the array is sorted
     array = array.sort((a, b) => a - b);
-    
+
     // capture the length of my array and the middle value index in variables
     let length = array.length;
     let middleIndex = Math.floor(array.length / 2);
@@ -22,23 +22,34 @@ class DataSet {
     //testcase, if it is an even or odd length of values, cos median would differ
 
     //if array length is odd
-    if (length % 2 !== 0){
-        median = array[middleIndex]
-        console.log(median);
-        return median
+    if (length % 2 !== 0) {
+      median = array[middleIndex];
+      console.log(median);
+      return median;
     }
     // if array length is even
     else {
-        median = (array[middleIndex] + array[middleIndex - 1]) / 2
-        console.log(median);
-        return median
+      median = (array[middleIndex] + array[middleIndex - 1]) / 2;
+      console.log(median);
+      return median;
     }
-}
+  }
 
-static calcMode(array){
-    
-}
-
+  static calcMode(array) {
+    //1. create an object to hold "element : no of times it appears in array"
+    mapElements = {};
+    //scan through my array
+    for (let element of array) {
+      //check if element (i.e key) ia already in the object
+      if (mapElements[element]) {
+        // if yes, add 1
+        mapElements[element]++;
+      } else {
+        // if no, set the value to 1
+        mapElements[element] = 1;
+      }
+    }
+  }
 }
 //DataSet.calcMean([1, 2, 5, 6, 7]);
 //DataSet.calcMedian([6,8,2,4,8,9,0]);
