311e5464f574a6da77b59492f3aacf0c39ed2a2e
Prevail Uzodinma <prevailuzodinma@gmail.com>
2024-01-26 11:09:22 +0100

    created class and added static attributes

---
+++ class.js
@@ -0,0 +1,8 @@
+/*Create a class that has class/static properties and methods.
+Show how to use/access them.*/
+
+class Mammal {
+    //class with static properties
+    static liveBirth = true;
+    static vertebrate = true;
+}
