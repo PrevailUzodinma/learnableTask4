98220f997b31173a3f6a73144f14a98fbc1e2a5b
Prevail Uzodinma <prevailuzodinma@gmail.com>
2024-01-28 21:44:11 +0100

    created a method that handles renting of movies with the logic of if a movie is available or not

--- movieApi.js
+++ movieApi.js
@@ -4,4 +4,15 @@ class Movie {
     this.genre = genre;
     this.available = available;
   }
+
+  rentMovie() {
+    // check if the movie is available
+    if (this.available) {
+        //if it is available, set it to unavaible since it is to be rented
+      this.available = false;
+      console.log(`You've just rented "${this.title}". Enjoy your movie!`);
+    } else {
+      console.log(`Oops! sorry, "${this.title}" is currently unavailable.`);
+    }
+  }
 }
