fc4d7b5d2c8ef25e9001b1c5fab09f2b94575291
Prevail Uzodinma <prevailuzodinma@gmail.com>
2024-01-28 21:47:18 +0100

    I created another class that will handle the functionalities aroung the movies

--- movieApi.js
+++ movieApi.js
@@ -8,11 +8,17 @@ class Movie {
   rentMovie() {
     // check if the movie is available
     if (this.available) {
-        //if it is available, set it to unavaible since it is to be rented
+      //if it is available, set it to unavaible since it is to be rented
       this.available = false;
       console.log(`You've just rented "${this.title}". Enjoy your movie!`);
     } else {
       console.log(`Oops! sorry, "${this.title}" is currently unavailable.`);
     }
+  }
+}
+
+class MovieLibrary {
+  constructor() {
+    this.movies = [];
   }
 }
